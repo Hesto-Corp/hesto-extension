ce117dc47b741dc58ecea8e39b6cde486fbf8271
Shlok Sobti <shlok.sobti@gmail.com>
2024-10-10 14:41:16 -0400

    Move all trigger related things to a single function

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -19,14 +19,6 @@ chrome.runtime.onConnect.addListener((port) => {
   console.assert(port.name === 'popup_lifecycle');
   port.onDisconnect.addListener(() => {
     console.log('Port has been disconnected');
-
-    // Send a message to the content script
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0] && typeof tabs[0].id !== 'undefined') {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'close_popup' });
-      }
-    });
-
     clearTriggerPopup();
   });
 });
--- src/scripts/utils/stateManagement.ts
+++ src/scripts/utils/stateManagement.ts
@@ -1,38 +1,11 @@
 import { safeSendMessage } from '../utils/messaging'
 
-// export function savePopupDataToStorage(popupData: PopupData) {
-//   chrome.storage.local.set({ popupData }, () => {
-//     console.log('Popup data has been set in Chrome storage:', popupData);
-//   });
-// }
-// 
-// export function setToIdle() {
-//   console.log("Setting Popup State to Idle");
-// 
-//   chrome.storage.local.set({ popupData: { state: PopupState.Idle } }, () => {
-//     console.log("Popup state has been set to Idle in Chrome storage.");
-//   });
-// }
-// 
-// export enum PopupState {
-//   Idle,
-//   Detected,
-// }
-// 
-// export interface Product {
-//   name: string | null
-//   price: number | null
-//   currency: string | null
-//   url?: string
-// }
-// 
-// export interface PopupData {
-//   state: PopupState // Idle / Detected
-//   product?: Product
-// }
-
-// New
 export function clearTriggerPopup(){
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0] && typeof tabs[0].id !== 'undefined') {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'close_popup' });
+    }
+  });
   updateAppState(AppState.Idle);
 }
 
